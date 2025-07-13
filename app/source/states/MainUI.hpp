5beb0190fe992555bf6ff680cafd6b1ccc315c02
Daniel López Guimaraes <danielectra@outlook.com>
2025-07-13 02:39:14 +0100

    feat: Add PNID migration tool
    When pressing B on Nimbus, try to commit the ACT account before it gets
    removed upon restarting when upgrading from a previous version. Note
    that the user will have to upgrade Nimbus while the console is turned
    on, such as with using Universal-Updater or updating the files over FTP.
    Also add a few convenience features.

--- app/source/states/MainUI.hpp
+++ app/source/states/MainUI.hpp
@@ -7,5 +7,6 @@ namespace MainUI
     Result unloadAccount(MainStruct* mainStruct);
     Result switchAccounts(MainStruct* mainStruct, u8 friend_account_id);
     Result createAccount(MainStruct* mainStruct, u8 friend_account_id, NascEnvironment environmentId);
+    void migrateAccount(MainStruct *mainStruct);
     bool drawUI(MainStruct *mainStruct, C3D_RenderTarget* top_screen, C3D_RenderTarget* bottom_screen, u32 kDown, u32 kHeld, touchPosition touch);
 }
